ec372c9441cab0a38b00d34336edf603d180ec7b
Alex Williams <cs.alexwilliams@gmail.com>
2013-10-23 13:24:45 -0500

    Uploading Initial Project.
    Uploading the initial project that spans from the Slide-out Me nu demo.

---
+++ app/SidebarDemo/SidebarViewController.m
@@ -0,0 +1,77 @@
+//
+//  SidebarViewController.m
+//  SidebarDemo
+//
+//  Created by Simon on 29/6/13.
+//  Copyright (c) 2013 Appcoda. All rights reserved.
+//
+
+#import "SidebarViewController.h"
+#import "PhotoViewController.h"
+#import "SWRevealViewController.h"
+
+@interface SidebarViewController ()
+
+@property (nonatomic, strong) NSArray *menuItems;
+@end
+
+@implementation SidebarViewController
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.view.backgroundColor = [UIColor colorWithWhite:0.2f alpha:1.0f];
+    self.tableView.backgroundColor = [UIColor colorWithWhite:0.2f alpha:1.0f];
+    self.tableView.separatorColor = [UIColor colorWithWhite:0.15f alpha:0.2f];
+    
+    _menuItems = @[@"title", @"dashboard", @"meetings", @"map", @"space", @"settings", @"about"];
+    
+
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [self.menuItems count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    /* Old Code **
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    return cell; */
+    
+    NSString *CellIdentifier = [self.menuItems objectAtIndex:indexPath.row];
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    
+    return cell;
+    
+}
+
+@end
