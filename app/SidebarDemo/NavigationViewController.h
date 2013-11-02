19777930fb77729b128a6a19cd47f7cc0e91d970
Nathan Reale <nathan.reale@gmail.com>
2013-11-02 14:53:19 -0500

    Added ARKit library
    There were a ton of errors when I added the library, mostly from ARKit
    trying to call release on objects. I think we are using automatic
    garbage collection, so it should release things automatically. However,
    we may have a severe memory leak if that isn't true.
    
    Also, I can't test this without on device testing.

---
+++ app/SidebarDemo/NavigationViewController.h
@@ -0,0 +1,13 @@
+//
+//  NavigationViewController.h
+//  FORCE
+//
+//  Created by Nathan Reale on 11/2/13.
+//  Copyright (c) 2013 Middle Tennessee State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface NavigationViewController : UIViewController
+
+@end
