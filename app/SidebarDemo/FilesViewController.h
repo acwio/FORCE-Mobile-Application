690a2788ba3c7fb539c649c96b8969c3cf56f6ac
Nathan Reale <nathan.reale@gmail.com>
2013-11-03 11:26:44 -0600

    Added Files page.

---
+++ app/SidebarDemo/FilesViewController.h
@@ -0,0 +1,13 @@
+//
+//  FilesViewController.h
+//  FORCE
+//
+//  Created by Nathan Reale on 11/3/13.
+//  Copyright (c) 2013 Middle Tennessee State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FilesViewController : UITableViewController
+
+@end
