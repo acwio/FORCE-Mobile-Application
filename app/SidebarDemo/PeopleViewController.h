dcda83621bc41e6c8621ec1f48f4bf84517dddf1
Nathan Reale <nathan.reale@gmail.com>
2013-11-03 10:50:21 -0600

    Added the People page, which shows a list of all people.

---
+++ app/SidebarDemo/PeopleViewController.h
@@ -0,0 +1,13 @@
+//
+//  PeopleViewController.h
+//  FORCE
+//
+//  Created by Nathan Reale on 11/3/13.
+//  Copyright (c) 2013 Middle Tennessee State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PeopleViewController : UITableViewController
+
+@end
---
+++ app/SidebarDemo/PeopleViewController.m
@@ -0,0 +1,153 @@
+//
+//  PeopleViewController.m
+//  FORCE
+//
+//  Created by Nathan Reale on 11/3/13.
+//  Copyright (c) 2013 Middle Tennessee State University. All rights reserved.
+//
+
+#import "PeopleViewController.h"
+#import "SWRevealViewController.h"
+#import "DataClass.h"
+
+@interface PeopleViewController ()
+
+@end
+
+@implementation PeopleViewController
+
+NSMutableArray *people;
+
+- (id)initWithStyle:(UITableViewStyle)style
+{
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    DataClass *data=[DataClass getInstance];
+    people = data.people;
+
+    // Uncomment the following line to preserve selection between presentations.
+    // self.clearsSelectionOnViewWillAppear = NO;
+ 
+    // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+    // self.navigationItem.rightBarButtonItem = self.editButtonItem;
+}
+
+// This is called both on load and after returning to this view
+// The gestures need to be set both times, so that was moved here.
+- (void)viewDidAppear:(BOOL)animated
+{
+    // Set the gesture
+    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView
+{
+    // Return the number of sections.
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
+{
+    // Return the number of rows in the section.
+    return [people count];
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    static NSString *CellIdentifier = @"PersonCell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier/* forIndexPath:indexPath*/];
+    
+    // Configure the cell...
+    if (cell == nil) {
+        cell = [[UITableViewCell alloc] initWithStyle:UITableViewCellStyleDefault reuseIdentifier:CellIdentifier];
+    }
+    
+    UIImageView *imageView = (UIImageView *)[cell viewWithTag:1];
+    UILabel *titleLabel = (UILabel *)[cell viewWithTag:2];
+    UILabel *companyLabel = (UILabel *)[cell viewWithTag:3];
+    
+    Person *person = [people objectAtIndex:indexPath.row];
+    
+    titleLabel.text = person.name;
+    companyLabel.text = @"Company"/*person.company*/;
+    [imageView setImage:[UIImage imageNamed:@"calendar_photo.jpg"]];
+    
+    for (UIView *subview in self.searchDisplayController.searchBar.subviews) {
+        if ([subview isKindOfClass:NSClassFromString(@"UISearchBarBackground")]) {
+            [subview removeFromSuperview];
+            break;
+        }
+    }
+    
+    return cell;
+}
+
+/*
+// Override to support conditional editing of the table view.
+- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the specified item to be editable.
+    return YES;
+}
+*/
+
+/*
+// Override to support editing the table view.
+- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    if (editingStyle == UITableViewCellEditingStyleDelete) {
+        // Delete the row from the data source
+        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+    }   
+    else if (editingStyle == UITableViewCellEditingStyleInsert) {
+        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+    }   
+}
+*/
+
+/*
+// Override to support rearranging the table view.
+- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath
+{
+}
+*/
+
+/*
+// Override to support conditional rearranging of the table view.
+- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath
+{
+    // Return NO if you do not want the item to be re-orderable.
+    return YES;
+}
+*/
+
+/*
+#pragma mark - Navigation
+
+// In a story board-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+
+ */
+
+@end
