635d0d9a39acfac5247ba949814787d024ebc663
Nathan Perry <hacorie@gmail.com>
2013-11-04 13:12:17 -0600

    Added Gesture and Buttons to the top of About and Settings so that you could navigate away from those pages after clicking on them :D

--- app/SidebarDemo/SeetingsViewController.m
+++ app/SidebarDemo/SeetingsViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "SeetingsViewController.h"
+#import "SWRevealViewController.h"
 
 @interface SeetingsViewController ()
 
@@ -26,6 +27,25 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
+    
+    /* navigation bar button button */
+    UIButton *btn =  [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn setFrame:CGRectMake(10.0, 2.0, 25.5, 24.0)];
+    [btn setBackgroundImage:[UIImage imageNamed:@"Nav_Icon.png"] forState:UIControlStateNormal];
+    [btn addTarget:self.revealViewController action:@selector(revealToggle:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *barBtn = [[UIBarButtonItem alloc] initWithCustomView:btn];
+    
+    self.navigationItem.leftBarButtonItem=barBtn;
+    /* navigation bar button */
+    
+    /* search bar button */
+    UIButton *btn2 =  [UIButton buttonWithType:UIButtonTypeCustom];
+    [btn2 setFrame:CGRectMake(10.0, 2.0, 25.5, 24.0)];
+    [btn2 setBackgroundImage:[UIImage imageNamed:@"Search_Magnify_Icon.png"] forState:UIControlStateNormal];
+    [btn2 addTarget:self.navigationController.searchDisplayController action:@selector(rightRevealToggle:) forControlEvents:UIControlEventTouchUpInside];
+    UIBarButtonItem *barBtn2 = [[UIBarButtonItem alloc] initWithCustomView:btn2];
+    
+    self.navigationItem.rightBarButtonItem=barBtn2;
 
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
@@ -38,6 +58,12 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    // Set the gesture
+    [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
 }
 
 #pragma mark - Table view data source
