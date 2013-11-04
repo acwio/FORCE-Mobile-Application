0e49019d21c921bc9a4733f06860b4d3222bed06
Nathan Perry <hacorie@gmail.com>
2013-11-04 12:08:42 -0600

    Added About and Settings pages. No info in them yet, but they are there and connected properly on storyboard.

---
+++ app/SidebarDemo/AboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  AboutViewController.h
+//  FORCE
+//
+//  Created by nep2d on 11/4/13.
+//  Copyright (c) 2013 Middle Tennessee State University. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AboutViewController : UITableViewController
+
+@end
