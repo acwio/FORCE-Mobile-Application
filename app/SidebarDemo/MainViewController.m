653cefd619ef42473e6bbeb16291ea34e465cc00
Nathan Reale <nathan.reale@gmail.com>
2013-11-06 21:36:19 -0600

    Added a shake listener on the main view controller
    It opens the next meeting time wise and goes to the media tab.

--- app/SidebarDemo/MainViewController.m
+++ app/SidebarDemo/MainViewController.m
@@ -131,6 +131,8 @@ NSMutableArray *groups;
 
     // Set the gesture
     [self.view addGestureRecognizer:self.revealViewController.panGestureRecognizer];
+    
+    [self becomeFirstResponder];
 }
 
 - (void)didReceiveMemoryWarning
@@ -257,6 +259,32 @@ NSMutableArray *groups;
     // Push the new meeting page on top of the current page
     [(UINavigationController*)self.revealViewController.frontViewController pushViewController:stubController animated:YES];
     
+}
+
+-(BOOL)canBecomeFirstResponder {
+    return YES;
+}
+
+
+- (void) viewDidDisappear:(BOOL)animated {
+    [self resignFirstResponder];
+    [super viewDidDisappear:animated];
+}
+
+- (void)motionEnded:(UIEventSubtype)motion withEvent:(UIEvent *)event {
+    if (event.subtype == UIEventSubtypeMotionShake) {
+        MeetingTabBarController *stubController = [self.storyboard instantiateViewControllerWithIdentifier:@"MeetingTabBar"];
+        [stubController setSelectedIndex:4];
+        stubController.view.backgroundColor = [UIColor whiteColor];
+        
+        Meeting *meet = [[[groups objectAtIndex:0] objectForKey:@"data"] objectAtIndex:0];
+        stubController.title = meet.name;
+        stubController.meeting = meet;
+        
+        // Push the new meeting page on top of the current page
+        [(UINavigationController*)self.revealViewController.frontViewController pushViewController:stubController animated:YES];
+    }
+    [super motionEnded:motion withEvent:event];
 }
 
 
