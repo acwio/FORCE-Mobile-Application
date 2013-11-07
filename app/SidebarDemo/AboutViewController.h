f6e24d85e0f01a5ebc779c860766953812e08334
Nathan Perry <hacorie@gmail.com>
2013-11-06 21:25:29 -0600

    Changed the About page just a bit. Made it a table View…
    Need to get mugshots of all of us to put in place of the placeholders

--- app/SidebarDemo/AboutViewController.h
+++ app/SidebarDemo/AboutViewController.h
@@ -2,24 +2,12 @@
 //  AboutViewController.h
 //  FORCE
 //
-//  Created by nep2d on 11/4/13.
+//  Created by nep2d on 11/6/13.
 //  Copyright (c) 2013 Middle Tennessee State University. All rights reserved.
 //
 
 #import <UIKit/UIKit.h>
 
-@interface AboutViewController : UIViewController
-{
-
-}
-
-@property (nonatomic, strong) IBOutlet UILabel *authors;
-@property (nonatomic, strong) IBOutlet UILabel *name1;
-@property (nonatomic, strong) IBOutlet UILabel *desc1;
-@property (nonatomic, strong) IBOutlet UILabel *name2;
-@property (nonatomic, strong) IBOutlet UILabel *desc2;
-@property (nonatomic, strong) IBOutlet UILabel *name3;
-@property (nonatomic, strong) IBOutlet UILabel *desc3;
-
+@interface AboutViewController : UITableViewController
 
 @end
